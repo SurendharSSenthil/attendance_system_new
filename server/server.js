e5d326df600660ba7f9f25c6c01e93025fae5ab1
SurendharSSenthil <surendharsenthil257@gmail.com>
2024-09-12 11:26:47 +0530

    cron logs added

--- server/server.js
+++ server/server.js
@@ -1,15 +1,15 @@
-const express = require("express");
-const cors = require("cors");
-const colors = require("colors");
-const connectDB = require("./Config/db");
-const facultyRoutes = require("./Routes/facultyRoutes");
-const attendanceRoutes = require("./Routes/attendanceRoutes");
-const adminRoutes = require("./Routes/adminRoutes");
-const cron = require("node-cron");
-const createReportCollection = require("./Models/reportModel");
-const mongoose = require("mongoose");
+const express = require('express');
+const cors = require('cors');
+const colors = require('colors');
+const connectDB = require('./Config/db');
+const facultyRoutes = require('./Routes/facultyRoutes');
+const attendanceRoutes = require('./Routes/attendanceRoutes');
+const adminRoutes = require('./Routes/adminRoutes');
+const cron = require('node-cron');
+const createReportCollection = require('./Models/reportModel');
+const mongoose = require('mongoose');
 
-require("dotenv").config();
+require('dotenv').config();
 
 const port = process.env.PORT || 5000;
 
@@ -19,13 +19,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.set("trust proxy", true);
+app.set('trust proxy', true);
 
 app.use((req, res, next) => {
-	const currentDateTime = new Date().toLocaleString("en-IN", {
-		timeZone: "Asia/Kolkata",
+	const currentDateTime = new Date().toLocaleString('en-IN', {
+		timeZone: 'Asia/Kolkata',
 	});
-	const clientIp = req.headers["x-forwarded-for"] || req.ip;
+	const clientIp = req.headers['x-forwarded-for'] || req.ip;
 	const method = req.method;
 
 	const methodColors = {
@@ -43,13 +43,13 @@ app.use((req, res, next) => {
 		colorize(
 			`Request Method: ${method}, Request Path: ${
 				req.path
-			}, Request Time: ${currentDateTime}, Request IP: ${req.get("host")}`
+			}, Request Time: ${currentDateTime}, Request IP: ${req.get('host')}`
 		)
 	);
 
 	next();
 });
-cron.schedule("0 0 * * *", async () => {
+cron.schedule('0 0 * * *', async () => {
 	try {
 		const now = new Date();
 
@@ -59,16 +59,15 @@ cron.schedule("0 0 * * *", async () => {
 			.toArray();
 		const reportCollections = collections
 			.map((collection) => collection.name)
-			.filter((name) => name.startsWith("reports_"));
-
+			.filter((name) => name.startsWith('reports_'));
 		// Update reports in each report collection
 		for (const collectionName of reportCollections) {
 			const ReportCollection = createReportCollection(
-				collectionName.replace("reports_", "")
+				collectionName.replace('reports_', '')
 			);
-			console.log("cron called!!!", reportCollections);
+			console.log('cron called!!!', reportCollections);
 			// Update reports where the creation date is more than 7 days ago
-			await ReportCollection.updateMany(
+			const res = await ReportCollection.updateMany(
 				{
 					created_at: { $lte: new Date(now - 7 * 24 * 60 * 60 * 1000) },
 					freeze: true,
@@ -80,17 +79,18 @@ cron.schedule("0 0 * * *", async () => {
 			);
 			console.log(new Date(now - 7 * 24 * 60 * 60 * 1000));
 			console.log(ReportCollection);
+			console.log('result is : ', res);
 		}
 
-		console.log("CRON called - Reports updated successfully");
+		console.log('CRON called - Reports updated successfully');
 	} catch (err) {
-		console.error("Error updating reports:", err);
+		console.error('Error updating reports:', err);
 	}
 });
 
-app.use("/api/students", facultyRoutes);
-app.use("/api/attendance", attendanceRoutes);
-app.use("/api/admin", adminRoutes);
+app.use('/api/students', facultyRoutes);
+app.use('/api/attendance', attendanceRoutes);
+app.use('/api/admin', adminRoutes);
 
 // app.get("/api", console.log("Hello world"));
 
