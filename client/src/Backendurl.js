5aeee693e4aa1a675dde20a5f12abfcb75657395
SurendharSSenthil <surendharsenthil257@gmail.com>
2024-11-30 21:43:55 +0530

    build

--- client/src/Backendurl.js
+++ client/src/Backendurl.js
@@ -1,3 +1,4 @@
 // export const url = "http://192.168.58.167:5000/api";
-export const url = 'http://localhost:5000/api';
+// export const url = 'http://localhost:5000/api';
 // export const url = 'https://pg.tneaseatmatrix.com/api';
+export const url = 'http://13.127.4.103/api';
